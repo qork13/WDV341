9d4b757f950d7b1a0228a1655b52a76a1fc239c1
qork13 <57408249+qork13@users.noreply.github.com>
2021-01-19 20:09:23 -0600

    Add git definitions for 2.1 assignment

--- test.php
+++ test.php
@@ -1,5 +1,11 @@
-<? php
+<html>
+
+<p>Version Control Software: software that allows commits and pushes to be able to track changes to the code</p>
+<p>Add: Add files to local repo for use</p>
+<p>Commit: Commit local code changes to the file in the local repo</p>
+<p>Push: Push local code commits to the github repository to update the code in the repo</p>
+<p>Pull: Pull changes from remote repo into local repo</p>
+<p>Clone: Clone remote repo .git file to be able to create a copy on your local station</p>
 
 
-
-?>
+</html>
